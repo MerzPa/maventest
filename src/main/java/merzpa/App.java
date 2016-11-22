023ca29b49a338f93e9d5a5c16f0292960318df4
PatrikM <patrik.merz@hs-furtwangen.de>
2016-11-22 17:04:15 +0100

    added java doc

--- src/main/java/merzpa/App.java
+++ src/main/java/merzpa/App.java
@@ -1,6 +1,15 @@
 package merzpa;
 import java.util.Scanner;
+/**
+ * Diese Klasse gibt einen Eingabe-String in Großbuchstaben aus.
+ * @author patrik
+ *
+ */
 public class App {
+	/**
+	 * blablabla
+	 * @param args
+	 */
 	public static void main(String args[]) {
 	        System.out.println(new Scanner(System.in).nextLine().toUpperCase() );
 	}

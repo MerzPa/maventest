c8aa73eeb0610e04fbfb72314e2d0f78a703a2af
PatrikM <patrik.merz@hs-furtwangen.de>
2016-11-22 16:39:06 +0100

    src added

---
+++ src/main/java/merzpa/App.java
@@ -0,0 +1,7 @@
+package merzpa;
+import java.util.Scanner;
+public class App {
+	public static void main(String args[]) {
+	        System.out.println(new Scanner(System.in).nextLine().toUpperCase() );
+	}
+}
